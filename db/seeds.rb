93dc33a71e38ea36eb29e18a93a1d2d8b55ce87c
Charles Duporge <cduporge@ekylibre.com>
2020-11-30 18:14:51 +0100

    Seed now fills visits with 100 per hour during 4 days

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,9 +21,12 @@ ShortenedUrl.create!(full_url: "https://www.google.com/")
 ShortenedUrl.create!(full_url: "https://www.linguee.fr/")
 
 # Create shortened_urls
+current_date = DateTime.now.beginning_of_hour
 ShortenedUrl.all.each do |url|
-  100.times do |index|
-    date = Faker::Time.between(from: 1.hour.ago, to: DateTime.now)
-    UrlVisit.create!(shortened_url_id: url.id, created_at: date, updated_at: date)
+  (24*4).times do |hour_index|
+    100.times do
+      date = Faker::Time.between(from: (current_date + hour_index.hours), to: (current_date + (hour_index + 1).hours))
+      UrlVisit.create!(shortened_url_id: url.id, created_at: date, updated_at: date)
+    end
   end
 end
